16b1f0bbfd5edc0d1ce32d98e9e5d2f73e028d30
Jens <jens@localhost.localdomain>
2021-04-24 10:30:01 +0000

    shell 2nd parameter

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-/home/jens/src/tradinglist/venv/bin/python3 /home/jens/src/tradinglist/listbacktrade_supertrend.py --listname $1
+/home/jens/src/tradinglist/venv/bin/python3 /home/jens/src/tradinglist/listbacktrade_supertrend.py --listname $1 --parameterupdate $2
